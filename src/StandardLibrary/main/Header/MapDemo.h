2e2129b152ee409a2ab437e8a07feeac34e0b549
徐锦涛 <2351180282@qq.com>
2018-09-12 15:48:14 +0800

    mapper测试

---
+++ src/StandardLibrary/main/Header/MapDemo.h
@@ -0,0 +1,24 @@
+//
+// Created by 80216435 on 2018/9/11.
+//
+
+#ifndef BOOST_MAPDEMO_H
+#define BOOST_MAPDEMO_H
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class MapDemo {
+public:
+	// 添加元素
+	void add();
+	// 减小元素
+	void remove();
+
+
+
+};
+
+
+#endif //BOOST_MAPDEMO_H
---
+++ src/StandardLibrary/main/Source/MapDemo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 80216435 on 2018/9/11.
+//
+
+#include "MapDemo.h"
